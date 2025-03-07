983ea2b1a207f5732e1d6eaddad2701ac3728198
Varuu04 <vardhanahemsham22@gmail.com>
2025-03-07 14:11:46 +0530

    modified

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <div>
+            <div className=".App-Header-Row">
+            <div>React Store</div>
+            <div>Menu</div></div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App name="Cathy" age={23} />);
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App3 from './App3';
+import reportWebVitals from './reportWebVitals';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App3 />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
